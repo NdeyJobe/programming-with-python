3b90d4c37f61e997db5f6033be07ea77ba9b7b32
Ndey Isatou Jobe <isa2jobe@gmail.com>
2018-03-15 16:21:29 +0100

    Add draw_art function

--- mindstorms.py
+++ mindstorms.py
@@ -1,21 +1,14 @@
 import turtle
 
-def draw_shape ():
-    window = turtle.Screen ()
-    window.bgcolor ("red")
-    
-    brad = turtle.Turtle ()
-    brad.shape("turtle")
-    brad.color("green")
-    brad.speed("3")
+def draw_art(some_turtle):
     side = 4
     count = 0
     while count < side:
-        brad.forward(100)
-        brad.right(90)
-        count += 1
-  
+        some_turtle.forward(100)
+        some_turtle.right(90)
+        count += 1  
 
+def draw_figure (): 
     angie = turtle.Turtle()
     angie.circle(100)
     angie.shape("arrow")
@@ -31,8 +24,29 @@ def draw_shape ():
     sami.right (45)
     sami.shape ("circle")
     sami.color ("pink")
+    
+draw_figure ()
 
+def draw_shape ():
+    window = turtle.Screen ()
+    window.bgcolor ("red")
+    
+    brad = turtle.Turtle ()
+    brad.shape ("turtle")
+    brad.color ("green")
+    brad.speed ("3")
+    
+    for i in range (1, 37):
+        draw_art (brad)
+        brad.right (10)
     window.exitonclick()
+    
+draw_shape()
+
+    
+  
 
 
-draw_shape ()
+
+
+
